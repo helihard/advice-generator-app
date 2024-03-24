66084adb7b8a5d1219c4d73f477d850308978823
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-24 23:25:53 +0100

    starter pack from Frontend Mentor, added to React app

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Attribution from "./components/Attribution.jsx"
+
+function App() {
+  return <Attribution />
+}
+
+export default App
---
+++ src/components/Attribution.jsx
@@ -0,0 +1,13 @@
+function Attribution() {
+  return (
+    <div className="attribution">
+      Challenge by&nbsp;
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Your Name Here</a>.
+    </div>
+  )
+}
+
+export default Attribution
