166d5a3617283bbadd43f94146f88d2acc485181
Heli Hård Källoff <vierge.hard@gmail.com>
2024-03-25 02:00:05 +0100

    added fetch

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,17 +1,39 @@
+import { useState, useEffect } from "react"
+
 import PatternDividerMobile from "../images/pattern-divider-mobile.svg"
-//import PatternDividerDesktop from "../images/pattern-divider-desktop.svg"
+import PatternDividerDesktop from "../images/pattern-divider-desktop.svg"
 import IconDice from "../images/icon-dice.svg"
 
 function MainContainer() {
+  const [advice, setAdvice] = useState(null)
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice")
+      const result = await response.json()
+      setAdvice(result)
+    } catch (error) {
+      console.error("No advice")
+    }
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  const handleClick = () => fetchAdvice()
+
   return (
     <div id="container">
-      <div id="advice-no-div">advice #</div>
-      <div id="quote-div">
-        &quot;It is easy to sit up and take notice, what&apos;s difficult is
-        getting up and taking action.&quot;
-      </div>
+      {advice && (
+        <>
+          <div id="advice-no-div">advice #{advice.slip.id}</div>
+          <div id="quote-div">&quot;{advice.slip.advice}&quot;</div>
+        </>
+      )}
       <img src={PatternDividerMobile} id="pattern-divider-mobile" />
-      <button>
+      <img src={PatternDividerDesktop} id="pattern-divider-desktop" />
+      <button onClick={handleClick}>
         <img src={IconDice} />
       </button>
     </div>
